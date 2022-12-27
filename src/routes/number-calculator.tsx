f04774862dd33e7aadb7f1340e999906f333c9ea
shinShinji01 <egorovdmitry.spb@gmail.com>
2022-12-27 20:47:21 +0300

    Finished number calculator logic. Needs refactoring.

--- src/routes/number-calculator.tsx
+++ src/routes/number-calculator.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { InputsState } from '../data/types';
 import { FilterMaterialUnit } from '../data/materials';
 import { SecondaryHeader } from '../components/header';
@@ -6,20 +6,29 @@ import Inputs from '../components/calculator/inputs';
 import Select from '../components/UI/select';
 import { filterMaterials, materialsTypes } from '../data/materials';
 
-const inputsData = [
-  {
-    label: 'Закрыто (№) до начала работы',
-    id: 'initial_number',
-  },
-  {
-    label: 'Готово единиц (М)',
-    id: 'units_stock_s',
-  },
-  {
-    label: 'Готово единиц (Б)',
-    id: 'units_stock_l',
-  },
-];
+interface InputsData {
+  label: string;
+  id: string;
+}
+
+interface KeyNumObject {
+  [key: string]: number;
+}
+
+const generateInputsObject = (
+  materialType: string,
+  materialsData: FilterMaterialUnit[]
+) => {
+  const inputsData = [
+    {
+      label: 'Закрыто (№) до начала работы',
+      id: 'initial_number',
+    },
+  ];
+  const materials = getDefaultMaterialData(materialType, materialsData);
+  materials.forEach((mat) => inputsData.push({ label: mat.label, id: mat.id }));
+  return inputsData;
+};
 
 /**
  * Find default data of materials of the selected type
@@ -44,8 +53,32 @@ const calculateUnitArea = (unitData: FilterMaterialUnit) => {
   return area;
 };
 
+// Render calculation data to the output container
+const renderOutput = (output: KeyNumObject) => {
+  if (!output) return;
+  const outputNodes: React.ReactNode[] = [];
+
+  for (const mat in output) {
+    const node = (
+      <li key={mat}>
+        {mat}: {output[mat]}
+      </li>
+    );
+    outputNodes.push(node);
+  }
+
+  return outputNodes;
+};
+
 const NumberCalculator = () => {
   const [matType, setMatType] = useState('');
+  const [inputsData, setInputsData] = useState<InputsData[]>([]);
+  const [output, setOutput] = useState<KeyNumObject | null>(null);
+
+  useEffect(
+    () => setInputsData(generateInputsObject(matType, filterMaterials)),
+    [matType]
+  );
 
   // Generate options for material type select menu
   const selectOptions = materialsTypes.map((mat) => {
@@ -64,24 +97,34 @@ const NumberCalculator = () => {
    * @param data input values
    */
   const calculate = (data: InputsState) => {
-    const {
-      initial_number: initialNumber,
-      units_stock_s: unitsStockS,
-      units_stock_l: unitsStockL,
-    } = data;
-    console.log(data);
-
     // Selected material data
     const defaultUnitsData = getDefaultMaterialData(matType, filterMaterials);
 
-    // Get areas of selected units (single)
-    const areas: { [key: string]: number } = {};
+    // Areas of selected units (single)
+    const areas: KeyNumObject = {};
     defaultUnitsData.forEach((mat) => {
-      areas[mat.size] = calculateUnitArea(mat);
+      areas[mat.id] = calculateUnitArea(mat);
     });
 
-    // Calculate total area covered by stock
-    const areaCovered = null;
+    // Area covered by stock
+    const areaCoveredByStock: KeyNumObject = {};
+    for (const mat in areas) areaCoveredByStock[mat] = areas[mat] * +data[mat];
+
+    // Area covered by single set
+    const areaCoveredBySet: KeyNumObject = {};
+    const defaultAmounts: KeyNumObject = {};
+    defaultUnitsData.forEach(
+      (mat) => (defaultAmounts[mat.id] = mat.defaultAmount)
+    );
+    for (const mat in areas)
+      areaCoveredBySet[mat] = areas[mat] * defaultAmounts[mat];
+
+    // How many sets covered
+    const results: KeyNumObject = {};
+    for (const mat in areaCoveredBySet)
+      results[mat] = areaCoveredByStock[mat] / areaCoveredBySet[mat];
+
+    setOutput(results);
   };
 
   return (
@@ -92,6 +135,9 @@ const NumberCalculator = () => {
         {selectOptions}
       </Select>
       <Inputs onCalculate={calculate} inputsData={inputsData} />
+      <div>
+        <ul>{output && renderOutput(output)}</ul>
+      </div>
     </>
   );
 };
